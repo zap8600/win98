281dec2dded9301c60bfaf1e6f7cdde6be8919a7
zap8600 <95487746+zap8600@users.noreply.github.com>
2026-02-10 14:29:51 +0000

    wip

--- b64tofile.c
+++ b64tofile.c
@@ -11,4 +11,32 @@
 const char* base64_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 int main(int argc, char** argv) {
+    if(argc != 3) {
+        fprintf(stderr, "Usage: %s [base64 data file] [output file]\n", argv[0]);
+        return 1;
+    }
+
+    char* base64_file_name = argv[1];
+    char* output_file_name = argv[2];
+
+    FILE* base64_file = fopen(base64_file_name, "rb");
+    if(!base64_file) {
+        perror(base64_file_name);
+        return 1;
+    }
+
+    FILE* output_file = fopen(output_file_name, "wb");
+    if(!output_file) {
+        perror(output_file_name);
+        fclose(base64_file);
+        return 1;
+    }
+
+    fseek(base64_file, 0, SEEK_END);
+    long base64_size = ftell(base64_file);
+    fseek(base64_file, -2, SEEK_CUR);
+
+    if((base64_size < 4) || (base64_size % 4)) {
+        fprintf(stderr, "Invalid ");
+    }
 }
