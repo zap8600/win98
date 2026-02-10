59d87c1dbb6b6aceb503b573930462127dc66b8c
zap8600 <zapost86@gmail.com>
2026-02-10 00:11:54 -0600

    wip

--- utils/b64tofile.c
+++ utils/b64tofile.c
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <sys/types.h>
 
+const char* base64_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 int main(int argc, char** argv) {
     if(argc != 3) {
         fprintf(stderr, "Usage: %s [source file containing base64] [output file]\n", argv[0]);
@@ -30,6 +32,19 @@ int main(int argc, char** argv) {
     fseek(base64_file, 0, SEEK_END);
     ssize_t base64_size = ftell(base64_file);
     fseek(base64_file, 0, SEEK_SET);
+
+    if((base64_size == 0) || (base64_size % 4)) {
+        fprintf(stderr, "Invalid file size: %zd\n", base64_size);
+        fclose(output_file);
+        fclose(base64_file);
+        return 1;
+    }
+
+    char base64_in[4];
+    uint8_t decoded_out[3];
+    for(ssize_t i = 0; i < base64_size; i += 4) {
+        fread(base64_in, 4, 1, base64_file);
+    }
 
     fclose(base64_file);
     fclose(output_file);
