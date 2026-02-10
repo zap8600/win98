92db672f42bb23be7ee7181cd4ee2fcfc43bce30
zap8600 <95487746+zap8600@users.noreply.github.com>
2026-02-10 14:17:01 +0000

    restructure

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # win98
-Utilities for working with Windows 98
+
+Utilities to assist me in working with a Windows 98 machine
+
+## Notes
+
+* When using `owcc`, use `-bnt -mconsole` to specify a Windows 32-bit console application (the terget for Windows 98).
+* When using `owcc`, use `-std=c99` to specify C99.
---
+++ b64tofile.c
@@ -0,0 +1,14 @@
+// Convert base64 data to a file
+// Intended for me to be able to transfer files to a Windows 98 machine by hand if all else fails
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+const char* base64_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+int main(int argc, char** argv) {
+}
